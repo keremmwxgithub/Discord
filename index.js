1d18a5cfbd2093af75a92595b6e9600b104785c8
keremmwxgithub <deneme1201234@gmail.com>
2024-07-08 18:23:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,17 +23,17 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('123')
+    .setApplicationId('1')
     .setType('STREAMING')
     .setURL('https://www.youtube.com/watch?v=4yFiBhNIWYo') //Must be a youtube video link 
     .setState('✧˚ · . ᴀᴄᴛɪᴠᴇ 𝟸𝟺/𝟽')
     .setName(' ')
     .setDetails(`≡;- ꒰ ° ᴋᴇʀᴇᴍᴍ ꒱ 🩷 [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://www.youtube.com/watch?v=xJ2CoqlBGGI') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1100708631311286342/1259775833132892170/c00910e4b80844c9234300c8abf395d7.gif?ex=668ce92c&is=668b97ac&hm=ee83a40e0e62b79d3aba803c86d23ed84ca3003cd493bebc00766bdb9559f240&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('･ﾟ✧*:･ﾟ  ᴅᴏ ɴᴏᴛ ᴅɪsᴛᴜʀʙ') //Text when you hover the Large image
-    .setAssetsSmallImage(' ') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText(' ') //Text when you hover the Small image
+    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('') //Text when you hover the Small image
 
 
   client.user.setActivity(r);
