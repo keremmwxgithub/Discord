23a2db81e5e074aff898c54932ec91954f59692c
Keremm.wx <deneme1201234@gmail.com>
2024-07-31 12:29:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,17 +7,6 @@ const client = new Discord.Client({
 const keepAlive = require('./server.js');
 keepAlive();
 
-function formatTime() { //Credits to himika#0001 and never#0001
-  const date = new Date();
-  const options = {
-    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
-    hour12: true,
-    hour: 'numeric',
-    minute: 'numeric'
-  };
-  return new Intl.DateTimeFormat('en-US', options).format(date);
-}
-
 client.on('ready', async () => {
   console.clear();
   console.log(`${client.user.tag} - rich presence started!`);
@@ -25,33 +14,20 @@ client.on('ready', async () => {
   const r = new Discord.RichPresence()
     .setApplicationId('123')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=NJptMSsS4kU') //Must be a youtube video link 
+    .setURL('https://www.youtube.com/watch?v=NJptMSsS4kU') // Must be a YouTube video link
     .setState('✧˚ · . ᴀᴄᴛɪᴠᴇ 𝟸𝟺/𝟽')
     .setName('🌈')
-    .setDetails(`≡;- ꒰ ° ɴᴀᴠɪᴀ ꒱ 💛 [${formatTime()}]`)
+    .setDetails('≡;- ꒰ ° ɴᴀᴠɪᴀ ꒱ 💛') // Details without time
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1097132368441122879/1267874880049643560/7b98b0c22e972190b262d8713e97e9f4.png?ex=66aa5fff&is=66a90e7f&hm=d2b382db62a293510d17668ea1a8501ca3da4eeb72c6f0a12648067d9b7e3e43&') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('･ﾟ✧*:･ﾟ  ᴅᴏ ɴᴏᴛ ᴅɪsᴛᴜʀʙ') //Text when you hover the Large image
-    .setAssetsSmallImage(' ') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText(' ') //Text when you hover the Small image
+    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1097132368441122879/1267874880049643560/7b98b0c22e972190b262d8713e97e9f4.png?ex=66aa5fff&is=66a90e7f&hm=d2b382db62a293510d17668ea1a8501ca3da4eeb72c6f0a12648067d9b7e3e43&') // Large image link
+    .setAssetsLargeText('･ﾟ✧*:･ﾟ  ᴅᴏ ɴᴏᴛ ᴅɪsᴛᴜʀʙ') // Text when you hover over the large image
+    .setAssetsSmallImage(' ') // Small image link
+    .setAssetsSmallText(' ') // Text when you hover over the small image
     .addButton(' ', 'https://www.youtube.com/')
     .addButton(' ', 'https://www.youtube.com/');
 
-
   client.user.setActivity(r);
-  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
-
-  let prevTime = null;
-  setInterval(() => {
-    const newTime = formatTime();
-    if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
-      r.setDetails(newDetails);
-      client.user.setActivity(r);
-      prevTime = newTime;
-    }
-  }, 1000); // Update every second
+  client.user.setPresence({ status: "dnd" }); // dnd, online, idle, offline
 });
 
-const mySecret = process.env['TOKEN'];
-client.login(mySecret);
+client.login('YOUR_TOKEN'); // Make sure to replace 'YOUR_TOKEN' with your actual bot token
