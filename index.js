92482c89587159739257bea1c117563cfe797bd5
Keremm.wx <deneme1201234@gmail.com>
2024-07-31 13:04:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ client.on('ready', async () => {
     .setName('🌈')
     .setDetails('≡;- ꒰ ° ɴᴀᴠɪᴀ ꒱ 💛') // Details without time
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1097132368441122879/1267874880049643560/7b98b0c22e972190b262d8713e97e9f4.png?ex=66aa5fff&is=66a90e7f&hm=d2b382db62a293510d17668ea1a8501ca3da4eeb72c6f0a12648067d9b7e3e43&') // Large image link
+    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1097132368441122879/1267874880049643560/7b98b0c22e972190b262d8713e97e9f4.png?ex=66ab08bf&is=66a9b73f&hm=fc993bc98dc9e2381e1446e7e6e8e0cf947e9dbeb8cac353f2b3b1ba18b47c56&') // Large image link
     .setAssetsLargeText('･ﾟ✧*:･ﾟ  ᴅᴏ ɴᴏᴛ ᴅɪsᴛᴜʀʙ') // Text when you hover over the large image
-    .setAssetsSmallImage('') // Small image link
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1097132368441122879/1268147215839264809/60.png?ex=66ab5da1&is=66aa0c21&hm=cdb43bec5ae54a39d4b3bd422ce48e0d2f5a87ef7c60a8672f923e2c4be5ef78&') // Small image link
     .setAssetsSmallText(' ') // Text when you hover over the small image
     .addButton(' ', 'https://www.youtube.com/')
     .addButton(' ', 'https://www.youtube.com/');
