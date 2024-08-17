6e388f79c39a886504cff1d27caf209298ef605f
Keremm.wx <deneme1201234@gmail.com>
2024-08-17 11:19:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ client.on('ready', async () => {
     .setName('Why are you looking at my profile, n1gga?')
     .setDetails('`≡;- ꒰ ° ᴋɪɴɪᴄʜ  ꒱ 💚') // Details without time
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1097132368441122879/1272130921369178154/de3eb136fe74e4e4b9f80a77115a5a25.png?ex=66b9dbbe&is=66b88a3e&hm=b6964d0965c689d302d64a443b5f009d2bacea4a1653cfa856c2ccaa7a56938d&') // Large image link
+    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1097132368441122879/1274278315833950300/e47f0edbffd342213e906ffd21ddae95.png?ex=66c1aba9&is=66c05a29&hm=fd3dbb9b8b330d829726d3ba58efc2d3c9c1b2ffb63913ab5860f9c9d1fb0dbf&') // Large image link
     .setAssetsLargeText('･ﾟ✧*:･ﾟ  ᴅᴏ ɴᴏᴛ ᴅɪsᴛᴜʀʙ') // Text when you hover over the large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1097132368441122879/1272131406612660264/9c1ed9619bd2fef8ec83008f45d7d666.png?ex=66b9dc32&is=66b88ab2&hm=85fe208358ada5ce332f9b197a3321bf448a52e648abac38c66b5d619aa394ce&') // Small image link
+    .setAssetsSmallImage(' ') // Small image link
     .setAssetsSmallText(' ') // Text when you hover over the small image
     .addButton(' ', 'https://www.youtube.com/')
     .addButton(' ', 'https://www.youtube.com/');
